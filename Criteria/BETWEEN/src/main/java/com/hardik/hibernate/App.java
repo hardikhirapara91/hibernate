9c2c6474c9ecf33174c62d65d4a87551e2b529fc
hardikhirapara91 <hardikhirapara91@gmail.com>
2017-06-29 13:38:26 +0530

    Hibernate Criteria Restrictions.between method example.

---
+++ Criteria/BETWEEN/src/main/java/com/hardik/hibernate/App.java
@@ -0,0 +1,37 @@
+package com.hardik.hibernate;
+
+import java.util.List;
+
+import org.hibernate.Criteria;
+import org.hibernate.Session;
+import org.hibernate.criterion.Restrictions;
+
+import com.hardik.hibernate.model.Employee;
+import com.hardik.hibernate.util.HibernateUtil;
+
+/**
+ * App Main
+ *
+ */
+public class App {
+
+	@SuppressWarnings({ "deprecation", "unchecked" })
+	public static void main(String[] args) {
+
+		Session session = HibernateUtil.getSessionFactory().openSession();
+		session.beginTransaction();
+
+		// Criteria
+		Criteria criteria = session.createCriteria(Employee.class).add(Restrictions.between("salary", 3000, 5000));
+
+		List<Employee> employees = criteria.list();
+
+		for (Employee employee : employees) {
+			System.out.println(employee);
+		}
+
+		// Close Session
+		session.getTransaction().commit();
+		HibernateUtil.shutdown();
+	}
+}
